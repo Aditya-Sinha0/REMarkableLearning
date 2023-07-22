9e4d230ef2a5292f959ed073fbd307a530cf0fb3
Aditya Sinha <sinhaditya02@gmail.com>
2023-07-22 14:56:37 -0700

    Runtime optimizations, gitignore changes

--- model.py
+++ model.py
@@ -1,43 +1,47 @@
 import pyedflib
-import csv
 import pandas as pd
 import numpy as np
 import os
+from tqdm import tqdm
 
-    
-def export_psg_to_csv(edf_file_path, csv_file_path):
-    
-    #Error handling For If edf file does not exist
+def export_psg_to_csv(edf_file_path, csv_file_path, channel_labels=None):
+
     if not os.path.exists(edf_file_path):
         raise FileNotFoundError(f"The EDF file '{edf_file_path}' does not exist.")
 
     edf_file = pyedflib.EdfReader(edf_file_path)
 
     # Get the indices and labels of the selected channels
-    channel_labels = edf_file.getSignalLabels()
+    if channel_labels == None:
+        channel_labels = edf_file.getSignalLabels()
     channel_indices = [edf_file.getSignalLabels().index(name) for name in channel_labels]
 
-    csv_data = []
+    total_samples = edf_file.getNSamples()[0]
 
-    # Iterate through the signals and store the data in a list of lists
-    for i in range(edf_file.getNSamples()[0]):
-        timestamp = edf_file.getSampleFrequency(channel_indices[0]) * i
-        row_data = [timestamp] + [edf_file.readSignal(idx)[i] for idx in channel_indices]
-        csv_data.append(row_data)
+    # Calculate all timestamps at once using NumPy
+    timestamps = np.arange(total_samples) * edf_file.getSampleFrequency(channel_indices[0])
+
+    # Prepare an empty array to store the channel data
+    channel_data = np.empty((total_samples, len(channel_indices)), dtype=float)
+
+    # Use tqdm to show progress bar
+    for i in tqdm(range(0, total_samples, 1000), desc="Exporting PSG Data", unit=" row"):
+        channel_data[i] = [edf_file.readSignal(idx)[i] for idx in channel_indices]
 
     edf_file.close()
 
+    # Create the DataFrame with timestamps and channel data
+    df_data = np.column_stack((timestamps, channel_data))
+    df_columns = ['Timestamp'] + channel_labels
+    df = pd.DataFrame(df_data, columns=df_columns)
+
     # Write the data to the CSV file
-    with open(csv_file_path, 'w', newline='') as csvfile:
-        csvwriter = csv.writer(csvfile)
-        csvwriter.writerow(['Timestamp'] + channel_labels)  # Write header row
-        csvwriter.writerows(csv_data)  # Write data rows
+    df.to_csv(csv_file_path, index=False)
 
     print(f"PSG data has been exported to {csv_file_path}.")
 
-
-# DRIVER
+# Example usage:
 edf_file_path = 'data\DatabaseSubjects\subject1.edf'
 csv_file_path = 'data\CSV_Data\subject1_data.csv'
 
-export_psg_to_csv(edf_file_path, csv_file_path)
+export_psg_to_csv(edf_file_path, csv_file_path, ['ECG', 'SAO2'])
